96419d5f2128e86d7222b934548c39a1bae274e9
Tao Huang <thuang@unc.edu>
2022-12-27 14:14:52 -0800

    Update CardInfo Design

--- src/Pages/CardInfo.js
+++ src/Pages/CardInfo.js
@@ -4,6 +4,20 @@ import 'react-credit-cards/es/styles-compiled.css'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa'
+import Discover from '../Images/Discover.png'
+import Boa321 from '../Images/boa321.png'
+import Cfu from '../Images/cfu.png'
+import Cff from '../Images/cff.png'
+import Csp from '../Images/csp.png'
+import Swa from '../Images/swa.png'
+import Mbb from '../Images/mbb.png'
+import Ue from '../Images/ue.png'
+import Ag from '../Images/amexGold.jpeg'
+import Abcp from '../Images/amexBcp.jpeg'
+import Aplat from '../Images/amexPlat.jpeg'
+import Adg from '../Images/amexDeltaGold.jpeg'
+import Ccc from '../Images/citicc.png'
+import Cp from '../Images/citip.png'
 
 const CardInfo = ({ infoCard }) => {
   const [number, setNumber] = useState('')
@@ -84,7 +98,7 @@ const CardInfo = ({ infoCard }) => {
   return (
     <div id="PaymentForm">
         <h2> {infoCard.type} </h2>
-        { checkInfo() ? '' :
+        {checkInfo() ? '' :
         <Cards
           cvc={cvc}
           expiry={expiry}
@@ -144,6 +158,24 @@ const CardInfo = ({ infoCard }) => {
             <button id='cardInputBtn'> <h2> Add My Card Information </h2> </button>
           </div>
         </form> }
+        { checkInfo() ? 
+          <div>
+            {infoCard.type === 'Discover It' ? <img className='cardInfoimage' src={Discover} alt='Discover It' width={350} height={200} /> : ''}
+            {infoCard.type === 'BOA 321' ? <img className='cardInfoimage' src={Boa321} alt='BOA 321' width={350} height={200} /> : ''}
+            {infoCard.type === 'CHASE FREEDOM UNLIMITED' ? <img className='cardInfoimage' src={Cfu} alt='CHASE FREEDOM UNLIMITED' width={350} height={200} /> : ''}
+            {infoCard.type === 'CHASE FREEDOM FLEX' ? <img className='cardInfoimage' src={Cff} alt='CHASE FREEDOM FLEX' width={350} height={200} /> : ''}
+            {infoCard.type === 'CHASE SAPPHIRE PREFERRED' ? <img className='cardInfoimage' src={Csp} alt='CHASE SAPPHIRE PREFERRED' width={350} height={200} /> : ''}
+            {infoCard.type === 'SOUTHWEST RAPID REWARDS® PLUS' ? <img className='cardInfoimage' src={Swa} alt='SOUTHWEST RAPID REWARDS® PLUS' width={350} height={200} /> : ''}
+            {infoCard.type === 'MARRIOTT BONVOY BOUNDLESS®' ? <img className='cardInfoimage' src={Mbb} alt='MARRIOTT BONVOY BOUNDLESS®' width={350} height={200} /> : ''}
+            {infoCard.type === 'UNITED EXPLORER' ? <img className='cardInfoimage' src={Ue} alt='UNITED EXPLORER' width={350} height={200} /> : ''}
+            {infoCard.type === 'American Express® Gold Card' ? <img className='cardInfoimage' src={Ag} alt='American Express® Gold Card' width={350} height={200} /> : ''}
+            {infoCard.type === 'Blue Cash Preferred® Card' ? <img className='cardInfoimage' src={Abcp} alt='Blue Cash Preferred® Card' width={350} height={200} /> : ''}
+            {infoCard.type === 'American Express Platinum Card' ? <img className='cardInfoimage' src={Aplat} alt='American Express Platinum Card' width={350} height={200} /> : ''}
+            {infoCard.type === 'Delta SkyMiles® Gold American Express Card' ? <img className='cardInfoimage' src={Adg} alt='Delta SkyMiles® Gold American Express Card' width={250} height={150} /> : ''}
+            {infoCard.type === 'Citi Custom Cash Card' ? <img className='cardInfoimage' src={Ccc} alt='Citi Custom Cash Card' width={350} height={200} /> : ''}
+            {infoCard.type === 'Citi Premier® Credit Card' ? <img className='cardInfoimage' src={Cp} alt='Citi Premier® Credit Card' width={350} height={200} /> : ''}
+          </div> : ''
+        }
         <div className={ checkInfo() ? 'infoYes' : '' }>
           <div>
             <h2 id='infoDes'> {info.map((card) => (card.id === infoCard.id ? 'Your Card Information:' : ''))} </h2>
